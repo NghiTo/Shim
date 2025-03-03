0067a27f06d6ed05f402b164cd5b9a54f9beacca
Kenzy <tobanghi@gmail.com>
2025-03-03 15:36:09 +0700

    Create page not found UI

--- frontend/src/AppRouter.tsx
+++ frontend/src/AppRouter.tsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import Loading from "./components/shared/Loading";
+import ErrorFallback from "./components/shared/ErrorFallBack";
+import NotFound from "./components/shared/NotFound";
 
 const Home = lazy(() => import("./pages/Home"));
 
@@ -12,6 +14,11 @@ const router = createBrowserRouter([
         <Home />
       </Suspense>
     ),
+    errorElement: <ErrorFallback />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
   },
 ]);
 
