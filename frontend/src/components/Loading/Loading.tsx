4d0f78fcc7eb91024106e662d7b8c014268c8520
Kenzy <tobanghi@gmail.com>
2025-03-03 15:14:00 +0700

    Create Loading UI

---
+++ frontend/src/components/Loading/Loading.tsx
@@ -0,0 +1,15 @@
+import { LoadingOutlined } from "@ant-design/icons";
+import { Spin } from "antd";
+
+const Loading = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="text-center">
+        <Spin indicator={<LoadingOutlined style={{ fontSize: 48 }} spin />} />
+        <p className="text-base text-gray-700">Loading...</p>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
